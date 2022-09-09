e07bd729d05d413190e4720945e56e58bba8a5b7
Mariana Kolodzheieva <os-mari.kolodzheieva@rakuten.com>
2022-09-09 16:49:54 +0100

    Solve file stats exercise

--- 3-0-java-core/3-6-2-file-stats/src/main/java/com/bobocode/se/FileStats.java
+++ 3-0-java-core/3-6-2-file-stats/src/main/java/com/bobocode/se/FileStats.java
@@ -1,12 +1,31 @@
 package com.bobocode.se;
 
-import com.bobocode.util.ExerciseNotCompletedException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.counting;
 
 /**
  * {@link FileStats} provides an API that allow to get character statistic based on text file. All whitespace characters
  * are ignored.
  */
 public class FileStats {
+
+    private final Path filePath;
+    private FileStats(Path filePath) {
+        this.filePath = filePath;
+    }
+
     /**
      * Creates a new immutable {@link FileStats} objects using data from text file received as a parameter.
      *
@@ -14,7 +33,7 @@ public class FileStats {
      * @return new FileStats object created from text file
      */
     public static FileStats from(String fileName) {
-        throw new ExerciseNotCompletedException(); //todo
+        return new FileStats(getPath(fileName));
     }
 
     /**
@@ -24,7 +43,9 @@ public class FileStats {
      * @return a number that shows how many times this character appeared in a text file
      */
     public int getCharCount(char character) {
-        throw new ExerciseNotCompletedException(); //todo
+        return (int) getCharacterStream()
+            .filter(ch -> ch == character)
+            .count();
     }
 
     /**
@@ -33,7 +54,12 @@ public class FileStats {
      * @return the most frequently appeared character
      */
     public char getMostPopularCharacter() {
-        throw new ExerciseNotCompletedException(); //todo
+        return getCharacterStream()
+            .collect(Collectors.groupingBy(Function.identity(), counting()))
+            .entrySet()
+            .stream().max(Comparator.comparingLong(Map.Entry::getValue))
+            .orElseThrow(() -> new FileStatsException("Invalid input"))
+            .getKey();
     }
 
     /**
@@ -43,6 +69,33 @@ public class FileStats {
      * @return {@code true} if this character has appeared in the text, and {@code false} otherwise
      */
     public boolean containsCharacter(char character) {
-        throw new ExerciseNotCompletedException(); //todo
+        return getCharacterStream()
+            .anyMatch(ch -> ch == character);
+    }
+
+    private static Path getPath(String fileName) {
+        Objects.requireNonNull(fileName);
+        URL fileUrl = FileStats.class.getClassLoader().getResource(fileName);
+        if (fileUrl == null) {
+            throw new FileStatsException("Invalid file URL");
+        }
+        try {
+            return Paths.get(fileUrl.toURI());
+        } catch (URISyntaxException e) {
+            throw new FileStatsException("Invalid file URL", e);
+        }
+    }
+
+    private Stream<Character> getCharacterStream() {
+        return openFileLinesStream(filePath)
+            .map(string -> string.replaceAll(" ", ""))
+            .flatMap(s -> s.chars().mapToObj((ch) -> (char) ch));
+    }
+    private static Stream<String> openFileLinesStream(Path filePath) {
+        try {
+            return Files.lines(filePath);
+        } catch (IOException e) {
+            throw new FileStatsException("Cannot create stream of file lines", e);
+        }
     }
 }
